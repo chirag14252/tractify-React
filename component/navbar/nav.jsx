7e169d33ff38524b9b3615d7550dc6c9a65f3e38
chirag142452 <chi.chiragbhardwaj@gmail.com>
2023-08-27 14:11:49 +0530

    navbar 60%

---
+++ component/navbar/nav.jsx
@@ -0,0 +1,32 @@
+import "./nav.css"
+import logo from "./images/traction.png";
+import homesvg from "./images/home.svg";
+const Nav = () => {
+    const heading = ["SERVICES", "SOLUTION", "CONTACT US"];
+    return (
+        <>
+
+
+            <div className="nav-bar">
+                <div className="left-box">
+                    <div className="nav-logo"><img src={logo} height={100}></img></div>
+                    <div className="nav-child">
+                        <img src={homesvg} alt="" className="home-svg" />
+                        {
+                            heading.map((item, idx) => {
+                                return <div className="heading" key={idx}>{item}</div>
+                            })
+                        }
+                    </div>
+                </div>
+                <div className="button-Schedule">SCHEDULE A MEET</div>
+            </div>
+
+
+
+        </>
+
+    )
+}
+
+export default Nav;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Nav from '../component/navbar/nav.jsx'
+function App() {
+ 
+
+  return (
+   
+    <>
+    <Nav/>
+    </>
+   
+  )
+}
+
+export default App
