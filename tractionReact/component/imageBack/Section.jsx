31212307859db0ff01bdf096d27610a6b3a8182e
chirag142452 <chi.chiragbhardwaj@gmail.com>
2023-08-28 22:36:23 +0530

    section added

---
+++ tractionReact/component/imageBack/Section.jsx
@@ -0,0 +1,22 @@
+import "./section.css"
+
+const Section = ({ url }) => {
+    return (
+        <div className="image-background">
+            <div className="heading-1">
+                <div className="heading-2">
+                    <div>
+                       <span style={{color:"white"}}>Web & Mobile</span> 
+                    </div>
+                    <div>
+                        <span style={{color:`rgb(87,197,255)`}}>App Development</span>
+                    </div>
+                    <div className="para-1">
+                        Backed with Latest Technology to Deliver the best in class User experience
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Section;
---
+++ tractionReact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Nav from '../component/navbar/nav.jsx'
+import Section from '../component/imageBack/section'
+function App() {
+ const url = "./images/banner-home.jpg";
+  return (
+   
+    <>
+    <Nav/>
+    <Section/>
+    </>
+   
+  )
+}
+
+export default App
